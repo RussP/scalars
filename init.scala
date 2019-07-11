413565adcfe3cb81c65097d0334fd87dbdf6e2dd
Russell Paielli <Russ.Paielli@nasa.gov>
2019-07-11 16:48:52 -0700

    added license files

--- init.scala
+++ init.scala
@@ -1,1 +1,6 @@
+
+// This file is used by the calculator script to initialize the Scalar
+// REPL as a units-aware calculator. It may cause some compilation
+// warnings, but don't worry about it.
+
 import scalar_._, units_._, mathx_._
