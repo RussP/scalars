a07b696300ebae383522df953a8afae15dcf5d2e
Russell Paielli <Russ.Paielli@nasa.gov>
2019-11-29 19:31:14 -0800

    replaced runtime.exec with Process to handle more files without hanging. Also added plot title page option

--- src/main/PlotManager.scala
+++ src/main/PlotManager.scala
@@ -4,12 +4,10 @@ package tools_
 
 import types_._
 import java.io._
-
+import scala.sys.process.Process
 import collection.parallel.CollectionConverters._
 
 case class PlotManager(name: Text, dir: Text=".", startClean: Bool=true) {
-
-  val run = Runtime.getRuntime
 
   val testDir = new File(dir)
   val texFile = new File(dir, s"$name.tex")
@@ -26,7 +24,7 @@ case class PlotManager(name: Text, dir: Text=".", startClean: Bool=true) {
     delete1(".log") // ditto
     delete1(".tex") // ditto
 
-    deleteFiles(".ps") // delete in termediate postscript files
+    deleteFiles(".ps") // delete intermediate postscript files
     deleteFiles(".pdf") // delete individual pdf files for each plot
     if (numFiles > 1) deleteFiles(".dat")
     ()
@@ -42,74 +40,110 @@ case class PlotManager(name: Text, dir: Text=".", startClean: Bool=true) {
     for (file <- testDir.listFiles(filter)) file.delete
     }
 
-  def combinePlots(display: Bool=true, save: Bool=false): Unit = {
+  def packagePlots(title: Text="", author: Text="", intro: Text="",
+    showDate: Bool=false, margin: Text="1in", parsep: Text="0.3in",
+    display: Bool=true, save: Bool=false) =
+
+    combinePlots(title, author, intro, showDate, margin, parsep,
+        display, save)
+
+  def combinePlots(title: Text="", author: Text="", intro: Text="",
+    showDate: Bool=false, margin: Text="1in", parsep: Text="0.3in",
+    display: Bool=true, save: Bool=false): Unit = {
 
     val utag = "-utag1zqp" // unique tag to avoid name clash
 
-    val files = {
+    val fileNames = {
 
-      val files1 = testDir.listFiles.toList.map(_.getName)
+      val fileNames1 = testDir.listFiles.toList.map(_.getName)
         .filter(_.startsWith(name)).filter(_.endsWith(".dat"))
         .map(x=>replaceAtEnd(x,".dat",".pdf")).sorted
 
-      if (files1.isEmpty) { cleanup(); return }
+      if (fileNames1.isEmpty) { cleanup(); return }
 
       val sameName = s"$name.pdf"
-      val indx = files1.indexOf(sameName)
+      val indx = fileNames1.indexOf(sameName)
 
       if (indx >= 0) { // avoid name clash with output file name
         val newName = s"$name$utag.pdf"
         val sameFile = new File(sameName)
         val newFile = new File(newName)
         sameFile.renameTo(newFile)
-        files1.updated(indx, newName)
+        fileNames1.updated(indx, newName)
         }
 
-      else files1
+      else fileNames1
       }
 
-    printLatexHeader
+    printLatexHeader(title, author, intro, showDate, margin, parsep)
 
-    for (file <- files) out.println(s"\\plot{$file}")
+    for (fileName <- fileNames) if (fileName.count(_ == '.') > 1)
+      // pdflatex will choke on file name if it has more than one .
+      throw new RuntimeException(s"\n$fileName has more than one period\n")
+
+    for (fileName <- fileNames) out.println(s"\\plot{$fileName}")
     out.println("\n\\end{document}")
     out.close
 
-    for (file <- files.par) { // ".par" for parallel processing
-      val datFile = replaceAtEnd(file,".pdf",".dat").replace(utag,"")
-      val psFile = replaceAtEnd(file,".pdf",".ps")
-      run.exec(s"gracebat -printfile $psFile $datFile").waitFor
-      run.exec(s"ps2pdf $psFile").waitFor
+    for (fileName <- fileNames.par) { // ".par" for parallel processing
+      val datFile = replaceAtEnd(fileName,".pdf",".dat").replace(utag,"")
+      val psFile = replaceAtEnd(fileName,".pdf",".ps")
+      Process(s"gracebat -printfile $psFile $datFile").!
+      Process(s"ps2pdf $psFile").!
       }
 
-    run.exec(s"pdflatex $name").waitFor
+    Process(s"pdflatex $name").!
 
     if (display) displayPlots
-    if (not(save)) cleanup(files.length)
+
+    if (save) deleteFiles(".ps") else // save all intermediate files except .ps
+    cleanup(fileNames.length) // delete all but the final pdf output file
     }
 
   def displayPlots() = {
     val runningLocally = System.getenv("SSH_CLIENT") == null
-    if (runningLocally) run.exec(s"acroread $name.pdf")
+    if (runningLocally) Process(s"acroread $name.pdf").!
+    //if (runningLocally) Runtime.getRuntime.exec(s"acroread $name.pdf")
     }
 
-  private def printLatexHeader() = {
+  private def printLatexHeader(title: Text, author: Text, intro: Text,
+    showDate: Bool, margin: Text, parsep: Text) = {
+
+    val (m1, p1) = (margin, parsep)
 
     out.println("\\documentclass[landscape]{slides}")
     out.println("\\usepackage{graphicx}")
-    out.println("\\usepackage{epstopdf}")
-    out.println("")
+    out.println("\\usepackage{epstopdf}\n")
+
+    out.println("\\renewenvironment{quote}")
+    out.println("  {\\small\\list{}")
+    out.println(s"  {\\rightmargin=$m1 \\leftmargin=$m1 \\parsep=$p1}%")
+    out.println("  \\item\\relax}{\\endlist}\n")
+
+    out.println("\\newcommand{\\plot}[1]{")
+    out.println("  \\begin{slide}\\begin{center}")
+    out.println("    \\includegraphics[width=10.5in,angle=0]{#1}")
+    out.println("  \\end{center}\\end{slide}}\n")
+
     out.println("\\setlength{\\textwidth}{11in}")
     out.println("\\setlength{\\textheight}{8.5in}")
     out.println("\\setlength{\\topmargin}{-1.5in}")
     out.println("\\setlength{\\oddsidemargin}{-1in}")
     out.println("\\setlength{\\evensidemargin}{\\oddsidemargin}")
-    out.println("")
-    out.println("\\newcommand{\\plot}[1]{")
-    out.println("  \\begin{slide}\\begin{center}")
-    out.println("    \\includegraphics[width=10.5in,angle=0]{#1}")
-    out.println("  \\end{center}\\end{slide}}")
-    out.println("")
-    out.println("\\begin{document}\n")
+
+    if (title.nonEmpty) out.println(s"\n\\title{\\textbf{$title}}")
+    if (author.nonEmpty) out.println(s"\\author{$author}")
+    if (not(showDate)) out.println("\\date{}")
+
+    out.println("\n\\begin{document}\n")
+
+    if (title.nonEmpty) out.println("\\maketitle\n")
+
+    if (intro.nonEmpty) {
+      out.println("\\begin{quote}\n")
+      out.println(intro) // optional explanatory text
+      out.println("\n\\end{quote}\n")
+      }
     }
 
   private def replaceAtEnd(txt: Text, ext1: Text, ext2: Text): Text =
